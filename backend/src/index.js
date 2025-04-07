95caea2bf0aaeb400e92a78eb423b68995037f75
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-04-07 15:49:00 +0200

    feat(backend): :sparkles: create initial server

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes/index.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/api/v1/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
