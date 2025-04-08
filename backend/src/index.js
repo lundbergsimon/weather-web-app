c5c00a55a4fb9a8d9e60e2c97cd8eeca79892946
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-04-08 11:39:04 +0200

    revert(backend): :fire: remove middleware to parse url encoded request bodies (not needed at this point)

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware to parse JSON request bodies
-app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded request bodies
 app.use('/api/v1', routes);
 
 app.listen(PORT, () => {
