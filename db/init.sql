9aad54722fc6b8dfb2694c45300104d6344a68f4
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-04-09 13:18:08 +0200

    feat(database): :card_file_box: add init file for database

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE weather_app;
+
+\c weather_app;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
