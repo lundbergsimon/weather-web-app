ea1e4ccfa0bb5038fa789304c91a21f93dfca124
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-04-11 09:31:37 +0200

    feat(backend): :sparkles: add functionality to verifyToken middleware
    The function now verifies the token attaches the decodedToken to the request which is forwarded to the next function

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -1,10 +1,23 @@
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    req.token = token;
-    next();
+  if (authHeader && authHeader.split(' ').length === 2) {
+    const access_token = authHeader.split(' ')[1];
+    try {
+      const decodedToken = jwt.verify(access_token, process.env.JWT_SECRET);
+      req.user = decodedToken;
+      console.log('Decoded token:', decodedToken);
+      return next();
+    } catch (err) {
+      console.error('Error verifying token:', err);
+      return res.status(403).json({ message: 'Forbidden' });
+    }
   } else {
+    console.error('Invalid authorization header:', authHeader);
     res.status(403).json({ message: 'Forbidden' });
   }
 };
