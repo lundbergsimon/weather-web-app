843b3536e9e1322efa559f03fe8319df62de8fed
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-12 15:36:34 +0200

    ci: :green_heart: add live updates for Docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  /** For live updates */
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    }
+  }
 })
