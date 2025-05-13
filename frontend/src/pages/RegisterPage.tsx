640de8b025165e5e0bc017263c39c605319f577a
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-13 17:05:48 +0200

    refactor(frontend): :lipstick: update styling of RegisterPage

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,11 +1,19 @@
 export default function RegisterPage() {
-  return <div className="">
-    <form className="flex flex-col gap-4 justify-center items-center">
-      <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" />
-      <button type="submit">Register</button>
-    </form>
-  </div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="flex flex-col gap-4 justify-center w-90">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <button type="submit" className="mt-4">Register</button>
+        </div>
+      </form>
+    </div>
+  );
 }
