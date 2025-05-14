68f0a6190258931601653dfe6a44e72f758dee21
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-14 15:50:59 +0200

    feat: :construction: add form to RegisterPage

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,17 +1,69 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.password || !formData.confirmPassword) {
+      alert("Please fill in all fields");
+      return;
+    }
+
+    alert(formData.password);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="flex flex-col gap-4 justify-center w-90">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 justify-center w-90"
+      >
         <div className="flex flex-col gap-2">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={(event) =>
+              setFormData({ ...formData, email: event.target.value })
+            }
+          />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formData.password}
+            onChange={(event) =>
+              setFormData({ ...formData, password: event.target.value })
+            }
+          />
         </div>
         <div className="flex flex-col gap-2">
-          <button type="submit" className="mt-4">Register</button>
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            type="password"
+            name="confirm-password"
+            id="confirm-password"
+            value={formData.confirmPassword}
+            onChange={(event) =>
+              setFormData({ ...formData, confirmPassword: event.target.value })
+            }
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <button type="submit" className="mt-4">
+            Register
+          </button>
         </div>
       </form>
     </div>
