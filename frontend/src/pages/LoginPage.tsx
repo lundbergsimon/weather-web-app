902df064c303c04c1642b55115c6a20bc6e064b6
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-14 15:50:28 +0200

    feat(frontend): :sparkles: WIP: add form to LoginPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      alert("Please fill in all fields");
+      return;
+    }
+
+    alert(formData.password);
+  };
+
   return (
-    <div>
-      Login Page
+    <div className="flex justify-center items-center h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 justify-center w-90"
+      >
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={(event) =>
+              setFormData({ ...formData, email: event.target.value })
+            }
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formData.password}
+            onChange={(event) =>
+              setFormData({ ...formData, password: event.target.value })
+            }
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <button type="submit" className="mt-4">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
