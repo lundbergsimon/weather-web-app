8cb4bfbfa918e458ab151bab526e0fd781fc4d05
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-15 11:41:29 +0200

    feat(backend): enable CORS middleware in the Express app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
+import cors from 'cors';
 import express from 'express';
 import routes from './routes/index.js';
 
 const app = express();
 
+
 const PORT = process.env.PORT;
 
+app.use(cors()); // Enable CORS
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use('/api/v1', routes);
 
