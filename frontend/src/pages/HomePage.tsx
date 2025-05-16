66b18f4a28121aa7659a47b9613fd8124e2a0d58
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-16 14:35:18 +0200

    feat(HomePage): WIP: implement temp forecast data retrieval with button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,32 @@
+import { useMutation } from "@tanstack/react-query";
+import useApi from "../hooks/useApi";
+
 export default function HomePage() {
+  const axiosInstance = useApi();
+
+  const sendForecastRequest = async () => {
+    const response = await axiosInstance.get(
+      "/forecast?lon=18.063240&lat=59.334591"
+    );
+    if (response.status !== 200) {
+      throw new Error("Error fetching forecast data");
+    }
+    return response.data;
+  };
+
+  const { mutateAsync: getForecast } = useMutation({
+    mutationFn: sendForecastRequest
+  });
+
+  const handleClick = async () => {
+    await getForecast();
+  };
+
   return (
     <div>
-      Home Page
+      <button type="button" onClick={handleClick}>
+        Click Me
+      </button>
     </div>
-  )
+  );
 }
