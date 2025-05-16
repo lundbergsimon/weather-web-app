15ed5cc7f3215a41cc2f9b39c1e5a528e0d3284a
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-16 17:26:39 +0200

    feat(frontend): implement forecast data fetching and display in HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import useApi from "../hooks/useApi";
+import { ForecastData } from "../types";
 
 export default function HomePage() {
   const axiosInstance = useApi();
+  const [forecast, setForecast] = useState<Array<ForecastData> | null>(null);
 
   const sendForecastRequest = async () => {
     const response = await axiosInstance.get(
@@ -11,7 +14,9 @@ export default function HomePage() {
     if (response.status !== 200) {
       throw new Error("Error fetching forecast data");
     }
-    return response.data;
+    console.log(response.data);
+
+    setForecast(response.data.data);
   };
 
   const { mutateAsync: getForecast } = useMutation({
@@ -23,10 +28,22 @@ export default function HomePage() {
   };
 
   return (
-    <div>
+    <div className="w-screen">
       <button type="button" onClick={handleClick}>
         Click Me
       </button>
+      {forecast && (
+        <div className="flex gap-4 flex-wrap">
+          {forecast.map((item: ForecastData, index: number) => (
+            <div key={index} className="bg-zinc-800 p-4 rounded-md border border-zinc-700">
+              {/* <p>Date: {item.datetime}</p> */}
+              <h1>{item.temperature.values[0]} {"\u2103"}</h1>
+              <p>Wind Speed: {item.windSpeed.values} {item.windSpeed.unit}</p>
+              <p>Wind Direction: {item.windDirection.values[0]} {item.windDirection.unit}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
