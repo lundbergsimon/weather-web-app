d87aa09e8cf8be00b6bcf43fba460cf5b6304204
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-20 17:18:26 +0200

    feat(backend): add middleware to verify refresh tokens from cookies

---
+++ backend/src/middlewares/verifyRefreshToken.js
@@ -0,0 +1,21 @@
+const verifyRefreshToken = (req, res, next) => {
+  const refreshToken = req.cookies.refresh_token;
+  console.log("cookies:", req.cookies);
+  console.log("signedCookies:", req.signedCookies);
+  if (refreshToken) {
+    try {
+      const decodedToken = jwt.verify(refreshToken, process.env.JWT_SECRET);
+      req.user = decodedToken;
+      console.log("Decoded token:", decodedToken);
+      return next();
+    } catch (err) {
+      console.error("Error verifying refresh token:", err);
+      return res.status(403).json({ message: "Forbidden" });
+    }
+  } else {
+    console.error("No refresh token provided");
+    res.status(403).json({ message: "Forbidden" });
+  }
+};
+
+export default verifyRefreshToken;
