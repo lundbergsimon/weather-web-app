3e13443b30a09b3223b468afe0ec8f093e7f076c
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-20 17:18:05 +0200

    fix(backend): update CORS configuration and ensure cookie parsing middleware is applied

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import routes from './routes/index.js';
 
 const app = express();
 
-
 const PORT = process.env.PORT;
 
-app.use(cors()); // Enable CORS
 app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cookieParser()); // Middleware to parse cookies from requests
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+})); // Middleware to enable CORS
 app.use('/api/v1', routes);
 
 app.listen(PORT, () => {
