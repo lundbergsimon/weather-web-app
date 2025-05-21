425a9828fcd9a2efb3c51cbc0828d23252e33e18
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-21 11:05:42 +0200

    refactor(pages): simplify forecast data fetching in HomePage component using useQuery and display with ForecastTable component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,47 +1,54 @@
-import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import ForecastTable from "../components/ForecastTable";
 import useApi from "../hooks/useApi";
 import { ForecastData } from "../types";
 
 export default function HomePage() {
   const axiosInstance = useApi();
-  const [forecast, setForecast] = useState<Array<ForecastData> | null>(null);
 
-  const sendForecastRequest = async () => {
-    const response = await axiosInstance.get(
-      "/forecast?lon=18.063240&lat=59.334591"
-    );
-    if (response.status !== 200) {
-      throw new Error("Error fetching forecast data");
+  const { data: forecast, isLoading } = useQuery({
+    queryKey: ["forecast"],
+    queryFn: async () => {
+      const response = await axiosInstance.get(
+        "/forecast?lon=18.063240&lat=59.334591"
+      );
+      if (response.status !== 200) {
+        throw new Error("Error fetching forecast data");
+      }
+      const forecastData = response.data.data as ForecastData[];
+      const forecastMap = new Map<number, ForecastData[]>();
+      forecastData.forEach((item) => {
+        const date = new Date(item.datetime).getDate();
+        if (forecastMap.has(date)) {
+          forecastMap.get(date)?.push(item);
+        } else {
+          forecastMap.set(date, [item]);
+        }
+      });
+      console.log(forecastMap);
+      return forecastMap;
     }
-    console.log(response.data);
-
-    setForecast(response.data.data);
-  };
-
-  const { mutateAsync: getForecast } = useMutation({
-    mutationFn: sendForecastRequest
   });
-
-  const handleClick = async () => {
-    await getForecast();
-  };
 
   return (
     <div className="w-screen">
-      <button type="button" onClick={handleClick}>
-        Click Me
-      </button>
+      {isLoading && (
+        <div className="flex justify-center items-center h-screen">
+          <p>Loading...</p>
+        </div>
+      )}
       {forecast && (
-        <div className="flex gap-4 flex-wrap">
-          {forecast.map((item: ForecastData, index: number) => (
-            <div key={index} className="bg-zinc-800 p-4 rounded-md border border-zinc-700">
-              {/* <p>Date: {item.datetime}</p> */}
-              <h1>{item.temperature.values[0]} {"\u2103"}</h1>
-              <p>Wind Speed: {item.windSpeed.values} {item.windSpeed.unit}</p>
-              <p>Wind Direction: {item.windDirection.values[0]} {item.windDirection.unit}</p>
-            </div>
-          ))}
+        <div className="flex justify-center items-center">
+          <div className="inline-flex flex-col justify-center items-center gap-4">
+            {Array.from(forecast.entries()).map(([key, value]) => (
+              <>
+                <h1 className="text-2xl font-bold text-center">
+                  {new Date(value[0].datetime).toDateString()}
+                </h1>
+                <ForecastTable key={key} timeSeries={value} />
+              </>
+            ))}
+          </div>
         </div>
       )}
     </div>
