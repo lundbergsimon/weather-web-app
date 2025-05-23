b8efcaa6eea5598a104aa65b4b33bb8fddf545b0
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-23 12:37:07 +0200

    refactor(hooks): remove useAuth dependency and simplify access token retrieval

--- frontend/src/hooks/useRefreshAccessToken.ts
+++ frontend/src/hooks/useRefreshAccessToken.ts
@@ -1,19 +1,12 @@
 import { axiosPublic } from "../config/api";
-import useAuth from "./useAuth";
 
 const useRefreshAccessToken = () => {
-  const { setAuth } = useAuth();
-
   const refreshAccessToken = async () => {
     const response = await axiosPublic.post("/auth/refresh", {
       withCredentials: true,
     });
 
-    setAuth({
-      accessToken: response.data["access_token"],
-    });
-
-    return response.data.accessToken;
+    return response.data["access_token"];
   };
 
   return { refreshAccessToken };
