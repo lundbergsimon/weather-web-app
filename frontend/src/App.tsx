91609544b6e377b56919a8389f9b647cf5e9bd4c
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-26 13:17:35 +0200

    feat(frontend): add TopBar as a layout component and integrate it into the routing structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import PersistLogin from "./components/PersistLogin";
 import PrivateRoute from "./components/PrivateRoute";
+import TopBar from "./components/TopBar";
 import { AuthProvider } from "./context/AuthProvider";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -19,7 +20,9 @@ function App() {
             <Routes>
               <Route element={<PersistLogin />}>
                 <Route element={<PrivateRoute />}>
-                  <Route path="/" element={<HomePage />} />
+                  <Route element={<TopBar />}>
+                    <Route path="/" element={<HomePage />} />
+                  </Route>
                 </Route>
               </Route>
               <Route path="/login" element={<LoginPage />} />
