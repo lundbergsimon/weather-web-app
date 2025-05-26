8c0d885876d9d7e69eb8fe092c12b93f0b4c0055
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-26 15:12:00 +0200

    docs(services): improve documentation

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,5 +1,14 @@
 import pool from "../db/db.js";
 
+/**
+ * Inserts a refresh token into the database for a given user.
+ *
+ * @param {number} userId - The ID of the user to insert the refresh token for.
+ * @param {string} refreshToken - The refresh token to insert.
+ *
+ * @returns {Promise<Object>} The inserted refresh token object.
+ * @throws Will throw an error if the database query fails.
+ */
 async function insertRefreshToken(userId, refreshToken) {
   try {
     const results = await pool.query(
@@ -16,6 +25,13 @@ async function insertRefreshToken(userId, refreshToken) {
   }
 }
 
+/**
+ * Invalidates a refresh token for a given user in the database.
+ *
+ * @param {number} userId - The ID of the user to invalidate the refresh token for.
+ *
+ * @throws Will throw an error if the database query fails.
+ */
 async function invalidateRefreshToken(userId) {
   try {
     await pool.query(
@@ -28,6 +44,14 @@ async function invalidateRefreshToken(userId) {
   }
 }
 
+/**
+ * Validates a refresh token by checking its existence in the database.
+ *
+ * @param {string} refreshToken - The refresh token to validate.
+ *
+ * @returns {Promise<number|null>} The user ID associated with the refresh token if it is valid, otherwise null.
+ * @throws Will throw an error if the database query fails.
+ */
 async function validateRefreshToken(refreshToken) {
   try {
     const results = await pool.query(
@@ -37,7 +61,6 @@ async function validateRefreshToken(refreshToken) {
     if (results.rowCount === 0) {
       return null;
     }
-
     return results.rows[0].user_id;
   } catch (error) {
     console.error("Error validating refresh token:", error);
