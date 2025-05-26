e7a5ee61d87d5649154d2ed94113bace34614ba7
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-26 15:36:48 +0200

    refactor(middlewares): improve variable naming and enhance documentation for access token verification

--- backend/src/middlewares/verifyAccessToken.js
+++ backend/src/middlewares/verifyAccessToken.js
@@ -3,12 +3,21 @@ import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
+/**
+ * Verify that the request has a valid access token by checking the Authorization
+ * header. If the access token is valid, store the user object in the request.
+ * If the access token is invalid or missing, return a 403 Forbidden response.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function in the stack.
+ */
 const verifyAccessToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader && authHeader.split(' ').length === 2) {
-    const access_token = authHeader.split(' ')[1];
+    const accessToken = authHeader.split(' ')[1];
     try {
-      const decodedToken = jwt.verify(access_token, process.env.JWT_SECRET);
+      const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET);
       req.user = decodedToken;
       return next();
     } catch (err) {
