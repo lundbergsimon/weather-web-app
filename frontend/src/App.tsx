907f4242073de148c878618640ff1506cef263fd
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-28 10:53:41 +0200

    docs(App): add documentation for the main app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,18 @@ import RegisterPage from "./pages/RegisterPage";
 
 const queryClient = new QueryClient();
 
+/**
+ * The main app component, which wraps the entire app in providers for authentication
+ * and react-query caching, and defines the routes.
+ *
+ * The app is organized into three main routes: the home page (at the root URL), the
+ * login page (at /login), and the register page (at /register). The home page is
+ * wrapped in the PersistLogin component, which ensures that the user is logged in
+ * asuming they have a valid refresh token. The HomePage component is also wrapped
+ * in the PrivateRoute component, which redirects the user to the login page if they
+ * are not logged in. Finally, the home page is wrapped in the TopBar component,
+ * which displays a logout button when logged in.
+ */
 function App() {
   return (
     <div className="app">
