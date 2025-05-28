9b6a3eb417ca828b8103d34e2c2d16e81d18fca3
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-28 11:36:47 +0200

    docs(AuthProvider): fix formatting and add documentation for AuthContext

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 type AuthContextType = {
   auth: { accessToken: string };
   setAuth: (auth: { accessToken: string }) => void;
-}
+};
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
@@ -11,9 +11,22 @@ interface AuthProviderProps {
   children: React.ReactNode;
 }
 
+/**
+ * A React Context Provider that provides a state variable and
+ * an updating function for the user's access token.
+ *
+ * The `auth` state variable is an object with a single `accessToken`
+ * property, which is a string representing the user's access token.
+ *
+ * The `setAuth` function takes an object with an `accessToken` property
+ * and updates the `auth` state variable with that value.
+ *
+ * The `AuthProvider` component wraps the `children` component(s) and
+ * provides the `auth` and `setAuth` values to them via React Context.
+ */
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [auth, setAuth] = useState<{ accessToken: string }>({
-    accessToken: "",
+    accessToken: ""
   });
 
   return (
