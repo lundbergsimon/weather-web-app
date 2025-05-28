fff3218dd98a5a96200736b2b1181663fcca3889
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-28 10:54:05 +0200

    refactor(ForecastTable): improve code formatting and add documentation

--- frontend/src/components/ForecastTable.tsx
+++ frontend/src/components/ForecastTable.tsx
@@ -5,6 +5,11 @@ interface ForecastTableProps {
   timeSeries: ForecastData[];
 }
 
+/**
+ * A table component that displays the given time series of forecast data.
+ * The table has four columns: time, temperature, wind speed, and wind direction.
+ * The wind direction is visualized with a rotating arrow icon.
+ */
 const ForecastTable: React.FC<ForecastTableProps> = ({ timeSeries }) => {
   return (
     <table className="border-separate border-spacing-2 border border-gray-400 dark:border-gray-500 w-full">
@@ -19,15 +24,29 @@ const ForecastTable: React.FC<ForecastTableProps> = ({ timeSeries }) => {
       <tbody>
         {timeSeries.map((item: ForecastData, index: number) => (
           <tr key={index}>
-            <td>{new Date(item.datetime).toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit", hour12: false})}</td>
+            <td>
+              {new Date(item.datetime).toLocaleTimeString(undefined, {
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: false
+              })}
+            </td>
             <td>{item.temperature.values[0]}°C</td>
-            <td>{item.windSpeed.values[0]} {item.windSpeed.unit}</td>
-            <td><FaArrowDownLong style={{transform: `rotate(${item.windDirection.values[0]}deg)`}} /></td>
+            <td>
+              {item.windSpeed.values[0]} {item.windSpeed.unit}
+            </td>
+            <td>
+              <FaArrowDownLong
+                style={{
+                  transform: `rotate(${item.windDirection.values[0]}deg)`
+                }}
+              />
+            </td>
           </tr>
         ))}
       </tbody>
     </table>
   );
-}
+};
 
 export default ForecastTable;
