761fd236ddbbac959cfd1ce1742aa340eed98ebf
LundbergDev <136829507+lundbergsimon@users.noreply.github.com>
2025-05-28 11:01:12 +0200

    refactor(useRefreshAccessToken): update documentation and remove unused return value

--- frontend/src/hooks/useRefreshAccessToken.ts
+++ frontend/src/hooks/useRefreshAccessToken.ts
@@ -1,6 +1,13 @@
 import { axiosPublic } from "../config/api";
 import useAuth from "./useAuth";
 
+/**
+ * A custom hook that provides a function to refresh the access token.
+ *
+ * This hook is useful for refreshing the access token by making a request
+ * to the `/auth/refresh` endpoint. It updates the `AuthProvider`with the
+ * new access token when the refresh is successful.
+ */
 const useRefreshAccessToken = () => {
   const { setAuth } = useAuth();
 
@@ -8,7 +15,6 @@ const useRefreshAccessToken = () => {
     const response = await axiosPublic.post("/auth/refresh");
     const accessToken = response.data["access_token"];
     setAuth({ accessToken });
-    return accessToken;
   };
 
   return { refreshAccessToken };
