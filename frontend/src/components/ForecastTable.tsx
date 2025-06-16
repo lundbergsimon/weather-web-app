8177cb023df8671f81bf6ef3af4146b07c7834c2
Simon Lundberg <136829507+lundbergsimon@users.noreply.github.com>
2025-06-16 19:34:49 +0200

    style(ForecastTable): adjust table border spacing for improved layout

--- frontend/src/components/ForecastTable.tsx
+++ frontend/src/components/ForecastTable.tsx
@@ -12,7 +12,7 @@ interface ForecastTableProps {
  */
 const ForecastTable: React.FC<ForecastTableProps> = ({ timeSeries }) => {
   return (
-    <table className="p-2 border-separate border-spacing-2 border border-zinc-800 w-full bg-gradient-to-b from-secondary to-primary shadow-neutral-900 shadow-lg rounded-2xl">
+    <table className="p-2 border-separate border-spacing-2 border-spacing-x-4 border border-zinc-800 w-full bg-gradient-to-b from-secondary to-primary shadow-neutral-900 shadow-lg rounded-2xl">
       <thead>
         <tr>
           <th>Time</th>
